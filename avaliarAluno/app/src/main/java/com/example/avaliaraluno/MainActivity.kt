c860aef6c27222419ecbb333dfafa6edc1b59352
Leticia Souza <puppydev@icloud.com>
2020-04-07 21:50:53 -0300

    exercicio que nao funciona por algum motivo

--- avaliarAluno/app/src/main/java/com/example/avaliaraluno/MainActivity.kt
+++ avaliarAluno/app/src/main/java/com/example/avaliaraluno/MainActivity.kt
@@ -4,56 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_resultado.*
 
 class MainActivity : AppCompatActivity() {
 
+    // prof, meu v:View por algum motivo nao ta sendo usado, comparei codigo com meus amigos e rodei o deles aqui, com o mesmo código não funciona, não sei se é alguma configuração ou o que.. mas a logica creio que ta certa
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         // este código sempre será executado antes da tela abrir
-        // no caso, ele vai alterar o que está escrito no componente
-        // de id "tvInicio" no XML
-
-        val nome = etNome.text
-        val nota1 = etNota1.inputType
-        val nota2 = etNota2.inputType
-
-        val mensagem = ""
-        val imagem1 = "@drawable/imagem1"
-        val imagem2 = "@drawable/imagem2"
     }
 
-    fun irTela2(v: View) {
+    fun avaliarAluno(v:View) {
         // colocar essa função no onclick do botão no xml de layout
         val resultado = Intent(this, Resultado::class.java) // cria um intent mas nao realiza a ação
 
-        val nome = intent.extras!!.getString("nome")
-        val nota1 = intent.extras!!.getFloat("nota1")
-        val nota2 = intent.extras?.getFloat("nota2")
+        val nomes = etNome.text.toString()
+        val nota1 = etNota1.text.toString().toFloat()
+        val nota2 = etNota2.text.toString().toFloat()
 
-        if (nota1+nota2?.div(2)!! >= 6) {
-            tvTexto.text = "Parabéns, NOME! Aprovado!"
-            ivImagem.setImageDrawable(`drawable-mdpi`)
-        } else {
-            tvTexto.text = "${nome}, infelizmente, reprovado"
-        }
-
+        resultado.putExtra("nome", nomes)
+        resultado.putExtra("nota01", nota1)
+        resultado.putExtra("nota02", nota2)
         startActivity(resultado) // realiza a ação desejada
-        setContentView(R.layout.activity_main)
-    }
-
-    //fun cliqueLoko(v: View) {
-    // tvInicio.text = "E num é que clicou? :O"
-    //  tvInvejoso.text = tvInicio.text
-    //}
-
-    fun sairApp(v: View) {
-        finishAffinity()
-    }
-
-    fun sair(v: View) {
-        finish()
     }
 }
--- avaliarAluno/app/src/main/java/com/example/avaliaraluno/Resultado.kt
+++ avaliarAluno/app/src/main/java/com/example/avaliaraluno/Resultado.kt
@@ -2,13 +2,30 @@ package com.example.avaliaraluno
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_resultado.*
 
 class Resultado : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
-    }
 
+        val nome = intent.extras?.getString("nomes")
+        val nota01 = intent.extras?.getFloat("nota1")
+        val nota02 = intent.extras?.getFloat("nota2")
+
+
+        val media = (nota01!! + nota02!!)?.div(2)
+
+        if (media!! >= 6) {
+            tvTexto.text = "Parabéns, ${nome}! Aprovado!"
+            ivImagem.setImageResource(R.drawable.imagem1)
+        } else {
+            tvTexto.text = "${nome}, infelizmente, reprovado"
+            ivImagem.setImageResource(R.drawable.imagem2)
+        }
+    }
 
 }
